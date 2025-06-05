d6122c87d5ac5028ed6a0901fae8090da8efb82e
DanikGuun <80112309+DanikGuun@users.noreply.github.com>
2025-06-05 16:00:20 +0300

    init

---
+++ RikApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RikApp
+//
+//  Created by Данила Бондарь on 05.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
