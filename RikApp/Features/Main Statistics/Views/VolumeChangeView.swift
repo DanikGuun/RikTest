6ff8885b61868ef87531f055ba0dfc192a6234df
DanikGuun <80112309+DanikGuun@users.noreply.github.com>
2025-06-08 19:39:55 +0300

    UserPanelView cоздание

---
+++ RikApp/Features/Main Statistics/Views/VolumeChangeView.swift
@@ -0,0 +1,108 @@
+
+import UIKit
+import PinLayout
+
+public class VolumeChangeView: UIView {
+    
+    public var item = VolumeChangeItem() { didSet { itemHasUpdated() } }
+    
+    private let graphImageView = UIImageView()
+    private let valueLabel = UILabel()
+    private let arrowImageView = UIImageView()
+    private let subtitleLabel = UILabel()
+    
+    public convenience init(){
+        self.init(frame: .zero)
+    }
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        graphImageView.pin.start().vertically().aspectRatio(1.9).margin(PEdgeInsets(top: 15, left: 10, bottom: 15, right: 0))
+        valueLabel.pin.start(to: graphImageView.edge.end).top().sizeToFit().margin(PEdgeInsets(top: 15, left: 15, bottom: 0, right: 10))
+        arrowImageView.pin.topStart(to: valueLabel.anchor.topEnd).bottom(to: valueLabel.edge.bottom).aspectRatio(1)
+        subtitleLabel.pin.topStart(to: valueLabel.anchor.bottomStart).bottomEnd().margin(PEdgeInsets(top: 7, left: 0, bottom: 15, right: 0))
+    }
+    
+    private func setup() {
+        setupGraphImageView()
+        setupValueLabel()
+        setupArrowImageView()
+        setupSubtitleLabel()
+    }
+    
+    private func setupGraphImageView() {
+        addSubview(graphImageView)
+    }
+    
+    private func setupValueLabel() {
+        addSubview(valueLabel)
+        valueLabel.font = UIFont(name: "Gilroy-Bold", size: 20)
+    }
+    
+    private func setupArrowImageView() {
+        addSubview(arrowImageView)
+    }
+    
+    private func setupSubtitleLabel() {
+        addSubview(subtitleLabel)
+        subtitleLabel.font = UIFont(name: "Gilroy-Medium", size: 15)
+        subtitleLabel.textColor = .tertiaryLabel
+        subtitleLabel.numberOfLines = 3
+    }
+    
+    private func itemHasUpdated() {
+        valueLabel.text = "\(item.value)"
+        subtitleLabel.text = item.type.subtitle
+        graphImageView.image = item.type.graphImage
+        arrowImageView.image = item.type.arrowImage
+    }
+}
+
+public struct VolumeChangeItem {
+    public var type: ChangeType
+    public var value: Int
+    
+    public init(type: ChangeType = .increase, value: Int = 0) {
+        self.type = type
+        self.value = value
+    }
+    
+    public enum ChangeType {
+        case increase
+        case decrease
+        
+        public var graphImage: UIImage {
+            var name = ""
+            switch self {
+            case .increase: name = "IncreaseGraphic"
+            case .decrease: name = "DecreaseGraphic"
+            }
+            return UIImage(resource: ImageResource(name: name, bundle: .main))
+        }
+        
+        public var subtitle: String {
+            switch self {
+            case .increase: return "Новые наблюдатели в этом месяце"
+            case .decrease: return "Пользователей перестали за Вами наблюдать"
+            }
+        }
+        
+        public var arrowImage: UIImage {
+            var name = ""
+            switch self {
+            case .increase: name = "IncreaseArrow"
+            case .decrease: name = "DecreaseArrow"
+            }
+            return UIImage(resource: ImageResource(name: name, bundle: .main))
+        }
+        
+    }
+}
